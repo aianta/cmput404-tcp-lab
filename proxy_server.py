536449161fd288905a9f51047bc85abd91dda072
Alex <ianta@ualberta.ca>
2023-01-09 12:45:29 -0400

    commented proxy_server.py

--- proxy_server.py
+++ proxy_server.py
@@ -5,42 +5,64 @@ BYTES_TO_READ = 4096
 PROXY_SERVER_HOST = "127.0.0.1"
 PROXY_SERVER_PORT = 8080
 
+# Send some data(request) to host:port
 def send_request(host, port, request):
-    # Send request
+
+    # Create a new socket in with block to ensure it's closed once we're done
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        # Connect the socket to host:port 
         client_socket.connect((host,port))
+        # Send the request through the connected socket.
         client_socket.send(request)
         
-        # Assemble response 
+        # Assemble response, be careful here, recall that recv(bytes) blocks until it recieves data!
         data = client_socket.recv(BYTES_TO_READ)
         result = b'' + data
-        while(b"</html>" not in result):
+        while(b"</html>" not in result): # Keep reading data until we get an '</html>' tag
             data = client_socket.recv(BYTES_TO_READ)
             result += data
     
         # Return response
         return result
 
+# Handle an incoming connection that has been accepted by the server.
 def handle_connection(conn, addr):
     with conn:
         print(f"Connected by {addr}")
 
-        while True:
-            data = conn.recv(BYTES_TO_READ)
-            if not data:
+        while True: # While the client is keeping the socket open
+            data = conn.recv(BYTES_TO_READ) # read some data from the socket
+            if not data: # If the socket has been closed, break.
                 break
-            print(data)
-            response = send_request("www.google.com", 80, data)
-            conn.sendall(response)
+            print(data) # Otherwise, print the data to the screen
+            response = send_request("www.google.com", 80, data) # and send it as a request to www.google.com
+            conn.sendall(response) #return the response from www.google.com back to the client
         
 
 # Start single-threaded proxy server
 def start_server():
+    '''
+    Create the socket in 'with' block to ensure it gets auto-closed once 
+    we're done.
+    '''
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        # Bind the server to a specific host and port on this machine
         server_socket.bind((PROXY_SERVER_HOST,PROXY_SERVER_PORT))
+        '''
+        Allow us to reuse this socket address during linger, as well as other
+        implications. 
+
+        See: https://stackoverflow.com/questions/14388706/how-do-so-reuseaddr-and-so-reuseport-differ
+        '''
         server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         server_socket.listen(2) #Allow queuing of up to 2 connections
+        '''
+        Wait for an incomming connection, and when one arrives, accept it and 
+        create a new socket called 'conn' to interact with it.
+        '''
         conn, addr = server_socket.accept()
+
+        # Pass 'conn' off to handle_connection to do some logic
         handle_connection(conn, addr)
 
 # Start multi-threaded proxy server
